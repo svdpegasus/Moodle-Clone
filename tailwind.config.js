320a3321225a4107d469eebf371e36e6b1f094fa
svdpegasus <97672658+svdpegasus@users.noreply.github.com>
2022-07-12 11:09:23 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {  spacing: {
+      '60px': '60px',
+      '120px': '120px',
+      '86': '86px',
+    }},
+  },
+  plugins: [],
+}
